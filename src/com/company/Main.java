149877d6e9d126277c4e1490953e00ae517070d0
KurbakoAnastasiia <kurbako.anastasia@gmail.com>
2021-01-26 16:32:40 -0500

    my changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Batch8");
-        
+
+        System.out.println("i am making some changes in my code");
 
 
     }
